33a952ab5b03eefa9f9f2270ca031a7f13b2be3b
Alex Otero <152084340+alexoterol@users.noreply.github.com>
2025-07-14 14:32:44 +0000

    PWA configuration for Vite project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/weather-dashboard-dawm",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+         registerType: 'autoUpdate',
+         devOptions: {
+            enabled: true
+         }
+      })
+  ],
 })
